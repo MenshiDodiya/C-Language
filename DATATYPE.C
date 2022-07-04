c3b6290b56fe288427a5ea2cde126813ef385ff8
MenshiDodiya <71643811+MenshiDodiya@users.noreply.github.com>
2022-07-04 12:33:42 +0530

    Add files via upload

---
+++ DATATYPE.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+//printing the all datatypes value
+void main()
+{
+	/*
+	DataTypes
+	2435    Integer     %d
+	34.56   Float       %f
+	'c'     Character   %c
+	"Hello" String      %s
+	*/
+	clrscr();
+	printf("Integer = %d\n",4567);
+	printf("Float = %f\n",56.23);
+	printf("Character = %c\n",'H');
+	printf("String = %s\n","Hello");
+	getch();
+}
